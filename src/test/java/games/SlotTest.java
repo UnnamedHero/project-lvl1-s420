cca2e2a4efd57cda6fb7cf43b73fe94ab9ddbfa3
UnnamedHero <hero.nospam@gmail.com>
2018-09-24 15:00:09 +0300

    add tests

---
+++ src/test/java/games/SlotTest.java
@@ -0,0 +1,16 @@
+package games;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static common.TestUtils.fromSystemOutPrintln;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SlotTest {
+    @Test
+    @DisplayName("\"main\" method works correctly")
+    void testMain() {
+        assertThat(fromSystemOutPrintln(games.Slot::main))
+                .isEqualTo("Hello, World!");
+    }
+}
