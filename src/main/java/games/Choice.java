3205ca896691930f3d8a81a762ed0d4138f6a0cb
UnnamedHero <hero.nospam@gmail.com>
2018-10-03 12:04:21 +0300

    use log4j2 logger

--- src/main/java/games/Choice.java
+++ src/main/java/games/Choice.java
@@ -1,32 +1,35 @@
 package games;
 
 import java.io.IOException;
+import org.slf4j.Logger;
 
 public class Choice {
 
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(Choice.class);
+
     static char getCharacterFromUser() throws IOException {
         byte[] input = new byte[1 + LINE_SEPARATOR.length()];
         if (System.in.read(input) != input.length) {
-            throw new RuntimeException("Пользователь ввёл недостаточное кол-во символов");
+            return getCharacterFromUser();
         }
     return (char) input[0];
     }
 
     public static void main(final String... __) throws IOException {
-        System.out.printf("Выберите игру%n" +
-                "1 - Казино (единый баланс для всех игр)%n" +
-                "2 - \'Однорукий бандит\'%n" +
-                "3 - \'Пьяница\'%n" +
-                "4 - \'Очко\'%n");
+        log.info("Выберите игру:");
+        log.info("1 - Казино (единый баланс для всех игр");
+        log.info("2 - \'Однорукий бандит\'");
+        log.info("3 - \'Пьяница\'");
+        log.info("4 - \'Очко\'");
         switch (getCharacterFromUser()) {
             case '1': Casino.main(); break;
             case '2': Slot.main(); break;
             case '3': Drunkard.main(); break;
             case '4': BlackJack.main(); break;
             default:
-                System.out.println("Игры с таким номером нет");
+                log.info("Игры с таким номером нет");
         }
     }
 }
